e7f9d9ba1109a189475e4bc986d4bd7b3f9cd7ee
Milos <milospejic2001@gmail.com>
2022-08-27 12:30:48 +0200

    Added place order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import SignInScreen from './screens/SignInScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
   const { state,dispatch: ctxDispatch  } = useContext(Store);
@@ -77,6 +78,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/signin" element={<SignInScreen />} />
               <Route path="/signup" element={<SignUpScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
               <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/payment" element={<PaymentMethodScreen />} />
               <Route path="/cart" element={<CartScreen />} />
